0e7201532f694fa3feb15bed404e186c307819e3
LesHank <henrik.berglund@gmail.com>
2023-07-30 12:06:39 +0200

    Add submodule

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/post-routes.js';
+import dalleRoutes from './routes/dalle-routes.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+
+app.get('/', async (req, res) => {
+   res.send('Hello World----!');
+});
+
+const startServer = async () => {
+   try {
+      await connectDB(process.env.MONGODB_URL);
+      app.listen(process.env.PORT, () => {
+         console.log(
+            `Server is running on port http://localhost:${process.env.PORT}`
+         );
+      });
+   } catch (error) {
+      console.log(error);
+   }
+};
+
+startServer();
