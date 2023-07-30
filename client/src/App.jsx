4498c65a42c7f6c6c8ff067d9953deeabb7f54c9
LesHank <henrik.berglund@gmail.com>
2023-07-30 13:04:15 +0200

    handle slider function

---
+++ client/src/App.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useRef } from "react";
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import { logo } from "./assets";
+
+import { Home, CreatePost, Info } from "./pages";
+
+const App = () => {
+   const link1 = useRef(null);
+   const link2 = useRef(null);
+   const link3 = useRef(null);
+
+   function handleSlider(ref) {
+      const slider = document.getElementById("slider");
+      slider.style.width = `${ref.current.offsetWidth}px`;
+      slider.style.height = `${ref.current.offsetHeight + 24}px`;
+      slider.style.transform = `translateX(${ref.current.offsetLeft}px)`;
+   }
+
+   useEffect(() => {
+      handleSlider(link1);
+   }, []);
+
+   return (
+      <BrowserRouter>
+         <header className="w-full flex items-center justify-center bg-black sm:px-8 py-4 border-b border-b-purple-700">
+            <Link to="/">
+               <img src={logo} alt="logo" className="w-32 ml-4 object-contain mr-8" />
+            </Link>
+            <div className="relative">
+               <div className="absolute z-0 bg-purple-600 rounded-full transition-all duration-300" id="slider"></div>
+               <Link
+                  to="/"
+                  className="font-base bg-transparent rounded-xl"
+                  ref={link1}
+                  onClick={() => handleSlider(link1)}
+               >
+                  <button className="relative text-gray-100 z-10 px-4 py-2 text-xl font-semibold">Gallery</button>
+               </Link>
+               <Link
+                  to="/create"
+                  className="font-base bg-transparent rounded-xl"
+                  ref={link3}
+                  onClick={() => handleSlider(link3)}
+               >
+                  <button className="relative text-gray-100 z-10 px-4 py-2 text-xl font-semibold">Create</button>
+               </Link>
+               <Link
+                  to="/info"
+                  className="font-base bg-transparent rounded-xl"
+                  ref={link2}
+                  onClick={() => handleSlider(link2)}
+               >
+                  <button className="relative text-gray-100 z-10 px-4 py-2 text-xl font-semibold">Info</button>
+               </Link>
+            </div>
+         </header>
+
+         <main className="sm:p-8 px-4 py-8 w-full bg-gray-900 min-h-[calc(100vh-73px)]">
+            <Routes>
+               <Route path="/" element={<Home />} />
+               <Route path="/create" element={<CreatePost />} />
+               <Route path="/info" element={<Info />} />
+            </Routes>
+         </main>
+      </BrowserRouter>
+   );
+};
+
+export default App;
