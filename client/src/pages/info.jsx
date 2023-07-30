90b2ea399f8dbf51c74c48436f9300d5f91cb13e
LesHank <henrik.berglund@gmail.com>
2023-07-30 13:06:43 +0200

    add info page

---
+++ client/src/pages/info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Info() {
+   return (
+      <section className="max-w-5xl mx-auto mt-4">
+         <h1 className="font-extrabold text-purple-400 text-4xl">Info</h1>
+         <p className="text-gray-400 text-lg mt-8">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Quisquam, quos. Quisquam, quos.
+            Quisquam, quos. Quisquam, quos. Quisquam, quos. Quisquam, quos. Quisquam, quos. Quisquam, quos. Quisquam,
+            quos. Quisquam, quos.
+         </p>
+      </section>
+   );
+}
